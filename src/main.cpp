383b733410d3e31d8db49042140bff9bb224fec3
Apostolos Chalis <tolishalis@gmail.com>
2026-04-03 18:21:30 +0300

    Added MFLOPS and GFLOPS calculation and output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Author: Apostolos Chalis 2026 achalis@csd.auth.gr 
 #include <iostream> 
 #include <chrono> 
+#include <iomanip>
 
 #include "utils.hpp"
 #include "kernel_interface.hpp"
@@ -23,15 +24,21 @@ int main(int argc, char* argv[]) {
 	// DDOT kernel 
 	std::cout << "Generating vectors..." << std::endl; 
 	std::vector<double> vector1 = generate_vector(args.size, args.seed);
-	std::vector<double> vector2 = generate_vector(args.size, args.seed);
+	std::vector<double> vector2 = generate_vector(args.size, args.seed + args.size);
 
 	std::cout << "Calculating DDOT..." << std::endl; 
 	auto start_of_DDOT = std::chrono::high_resolution_clock::now();
 	double DDOT_calc = ddot_serial(vector1.data(), vector2.data(), args.size);
 	auto end_of_DDOT = std::chrono::high_resolution_clock::now();
-
+	
+	// Time calculation and output
 	std::chrono::duration<double> dur_of_DDOT = end_of_DDOT - start_of_DDOT;
 	std::cout << "Finished DDOT calculation at: (" << std::fixed << std::setprecision(6) << dur_of_DDOT.count() << "s)" << std::endl;
+
+	// MFLOPS & GFLOPS calculation and output
+	double mflops = (2.0 * args.size) / (dur_of_DDOT.count() * 1e6);
+	double gflops = mflops / 1e3;
+	std::cout << "DDOT Performance: " << std::fixed << std::setprecision(2) << mflops << " MFLOPS (" << std::fixed << std::setprecision(2) << gflops << " GFLOPS)" << std::endl;
 
     return EXIT_SUCCESS;
 }
