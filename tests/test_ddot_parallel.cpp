6ce23cf22de398ecdf559d3e8311f42b74483719
Apostolos Chalis <tolishalis@gmail.com>
2026-04-26 16:15:34 +0300

    Added test (ddot_parallel)

--- tests/test_ddot_parallel.cpp
+++ tests/test_ddot_parallel.cpp
@@ -3,11 +3,11 @@
 
 #include "kernel_interface.hpp"
 
-TEST_CASE("DOT Product math (serial) are correct", "[ddot_serial]") {
+TEST_CASE("DOT Product (parallel) math are correct", "[ddot_parallel]") {
 	std::vector<double> first_vector = {1.0, 2.0, 3.0};
 	std::vector<double> second_vector = {4.0, 5.0, 6.0}; 
 
-	double result = ddot_serial(first_vector.data(), second_vector.data(), 3);
+	double result = ddot_parallel(first_vector.data(), second_vector.data(), 3);
 	
 	REQUIRE(result == 32.0);
 }
